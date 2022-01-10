b7158bd40a93bc7ffc68f254cb279f0128cdb353
임주민 <limj1213@gmail.com>
2022-01-11 05:20:55 +0900

    ✅[CHORE] #1 - 코드 리뷰 반영 수정

--- Fillin-iOS/Fillin-iOS/Sources/ViewControllers/StudioMapViewController/StudioMapViewController.swift
+++ Fillin-iOS/Fillin-iOS/Sources/ViewControllers/StudioMapViewController/StudioMapViewController.swift
@@ -13,7 +13,7 @@ import SnapKit
 import Then
 import MapKit
 
-class StudioMapViewController: UIViewController, CLLocationManagerDelegate {
+class StudioMapViewController: UIViewController {
   
   // MARK: - Properties
   private let mapView = NMFNaverMapView(frame: .zero)
@@ -26,8 +26,8 @@ class StudioMapViewController: UIViewController, CLLocationManagerDelegate {
     $0.backgroundColor = .darkGrey2
     $0.layer.borderColor = UIColor.fillinRed.cgColor
     $0.layer.borderWidth = 1
-    $0.textColor = .grey2 // 검색했을 때 글씨색 질문
-    $0.font = UIFont(name: "NotoSansKR-Regular", size: 14)
+    $0.textColor = .white
+    $0.font = .body2
     $0.setPlaceHolder()
     $0.addLeftPadding()
   }
@@ -55,37 +55,39 @@ extension StudioMapViewController {
   
   private func setUpMapView() {
     view.addSubview(mapView)
+    mapView.mapView.positionMode = .direction
     mapView.mapView.mapType = .navi
     mapView.mapView.isNightModeEnabled = true
     mapView.showZoomControls = false
     mapView.showScaleBar = false
+    
+    locationManager.delegate = self
+    self.locationManager.requestWhenInUseAuthorization()
   }
   
   private func setUpMarker() {
     marker.position = NMGLatLng(lat: 37.45201087366944, lng: 126.65536562781361)
-    marker.iconImage = NMFOverlayImage(name: "icnPlaceBig")
+    marker.iconImage = NMFOverlayImage(image: Asset.icnPlaceBig.image)
     marker.mapView = self.mapView.mapView
   }
   
   private func layoutMapView() {
     mapView.snp.makeConstraints {
-      $0.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading)
       $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
-      $0.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing)
+      $0.leading.equalTo(self.view.safeAreaLayoutGuide.snp.leading)
       $0.bottom.equalTo(self.view.snp.bottom)
+      $0.trailing.equalTo(self.view.safeAreaLayoutGuide.snp.trailing)
     }
   }
   
   private func layoutMyLocationButton() {
     view.addSubview(myLocationButton)
-    myLocationButton.setImage(UIImage(named: "icnMyLocation"), for: .normal)
+    myLocationButton.setImage(Asset.icnMyLocation.image, for: .normal)
     myLocationButton.snp.makeConstraints {
       $0.top.equalTo(self.view).inset(708)
-      $0.left.equalTo(self.view).inset(299)
-      $0.right.equalTo(self.view).inset(20)
-      $0.bottom.equalTo(self.view).inset(48)
+      $0.leading.equalTo(self.view).inset(299)
     }
-    myLocationButton.addTarget(self, action: #selector(locationTapped), for: .touchUpInside)
+    myLocationButton.addTarget(self, action: #selector(touchLocationButton), for: .touchUpInside)
   }
   
   private func layoutSeachView() {
@@ -93,35 +95,29 @@ extension StudioMapViewController {
     view.addSubview(magnifyingGlassButton)
     searchPlaceTextField.snp.makeConstraints {
       $0.top.equalTo(self.view).inset(111)
-      $0.left.equalTo(self.view).inset(18)
-      $0.right.equalTo(self.view).inset(18)
+      $0.leading.equalTo(self.view).inset(18)
+      $0.trailing.equalTo(self.view).inset(18)
       $0.size.height.equalTo(48)
     }
     magnifyingGlassButton.snp.makeConstraints {
       $0.top.equalTo(searchPlaceTextField).inset(11)
-      $0.left.equalTo(searchPlaceTextField).inset(295)
-      $0.right.equalTo(searchPlaceTextField).inset(18)
+      $0.leading.equalTo(searchPlaceTextField).inset(295)
       $0.bottom.equalTo(searchPlaceTextField).inset(11)
+      $0.trailing.equalTo(searchPlaceTextField).inset(18)
     }
-    magnifyingGlassButton.addTarget(self, action: #selector(searchTapped), for: .touchUpInside)
+    magnifyingGlassButton.addTarget(self, action: #selector(touchSearchButton), for: .touchUpInside)
   }
 }
 
 // MARK: - @objc
 extension StudioMapViewController {
   
-  @objc func locationTapped(_ sender: UIButton) {
-    if sender.isSelected == true {
-      sender.isSelected = false
-      mapView.mapView.positionMode = .direction
-    } else {
-      sender.isSelected = true
-      mapView.mapView.positionMode = .direction
-    }
+  @objc func touchLocationButton(_ sender: UIButton) {
+    sender.isSelected = !sender.isSelected
+    mapView.mapView.positionMode = .direction
   }
   
-  @objc func searchTapped(_ sender: UIButton) {
-    /// 이부분은 뷰 더 나오면 나중에 구현
+  @objc func touchSearchButton(_ sender: UIButton) {
   }
 }
 
@@ -134,11 +130,36 @@ extension UITextField {
     self.leftViewMode = ViewMode.always
   }
   
-  func setPlaceHolder() { // 폰트 적용 안됨 문제 ㅜㅅㅜ
+  func setPlaceHolder() {
     let attributes = [
-      NSAttributedString.Key.foregroundColor: UIColor.grey2
-      // NSAttributedString.Key.font: UIFont(name: "NotoSansKR-Regular", size: 14)!
+      NSAttributedString.Key.foregroundColor: UIColor.grey2,
+      NSAttributedString.Key.font: UIFont(name: "NotoSansKR-Regular", size: 14)!
+      //NSAttributedString.Key.font: .body2
     ]
     self.attributedPlaceholder = NSAttributedString(string: "추억을 현상할 현상소를 검색해보세요", attributes: attributes)
+  }
+}
+
+// MARK: - Extension - CLLocationManagerDelegate
+extension StudioMapViewController: CLLocationManagerDelegate {
+  
+  func getLocationUsagePermission() {
+    self.locationManager.requestWhenInUseAuthorization()
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    switch status {
+    case .authorizedAlways, .authorizedWhenInUse:
+      print("GPS 권한 설정됨")
+      self.locationManager.startUpdatingLocation()
+    case .restricted, .notDetermined:
+      print("GPS 권한 설정되지 않음")
+      getLocationUsagePermission()
+    case .denied:
+      print("GPS 권한 요청 거부됨")
+      getLocationUsagePermission()
+    default:
+      print("GPS: Default")
+    }
   }
 }
