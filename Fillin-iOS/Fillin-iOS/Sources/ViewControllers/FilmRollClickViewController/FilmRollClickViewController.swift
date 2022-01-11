a629e7f80b34c8b585bc62fd2c54886fbae2cde6
dlwns33 <choi990406@gmail.com>
2022-01-11 20:47:24 +0900

    ✨[FEAT] #8 - 아울렛 다시 정리하기

--- Fillin-iOS/Fillin-iOS/Sources/ViewControllers/FilmRollClickViewController/FilmRollClickViewController.swift
+++ Fillin-iOS/Fillin-iOS/Sources/ViewControllers/FilmRollClickViewController/FilmRollClickViewController.swift
@@ -9,30 +9,48 @@ import UIKit
 
 class FilmRollClickViewController: UIViewController {
 
-    // MARK: - Properties
+    // MARK: - @IBOutlet Properties
     @IBOutlet weak var dimmedBackview: UIView!
     @IBOutlet weak var popUpView: UIView!
     
-    @IBOutlet weak var profileTouchArea: UIView!
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var profileNameLabel: UILabel!
-    
-    @IBOutlet weak var filmTouchArea: UIView!
     @IBOutlet weak var filmNameLabel: UILabel!
     
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
     
-    @IBOutlet var dividerViews: [UIView]!
-    
+    // MARK: - @IBAction Properties
     @IBAction func touchDismissButton(_ sender: Any) {
-        
+        UIView.animate(withDuration: 0.2, delay: 0, options: .curveEaseOut, animations: {
+            self.dimmedBackview.alpha = 0.0
+            self.view.layoutIfNeeded()
+            self.popUpView.isHidden = true
+        }, completion: { _ in
+            if self.presentingViewController != nil {
+                self.dismiss(animated: false, completion: nil)
+            }
+        })
     }
     
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        setUI()
     }
 
+}
+
+// MARK: - Extensions
+extension FilmRollClickViewController {
+    private func setUI() {
+        profileNameLabel.font = .body2
+        filmNameLabel.font = .engDisplay2Book
+        likeButton.titleLabel?.font = .body2
+        likeButton.layer.borderWidth = 1
+        likeButton.layer.borderColor = UIColor.darkGrey1.cgColor
+//        likeButton.alignTextBelow()
+//        likeButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    
 }
