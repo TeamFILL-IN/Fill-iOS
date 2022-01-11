89d82f07d81740f07adc3261974ca81f6e9d2be5
dlwns33 <choi990406@gmail.com>
2022-01-11 20:22:53 +0900

    ➕[ADD] #8 - UIButton Extension 추가

---
+++ Fillin-iOS/Fillin-iOS/Resources/Extensions/UIButton+Extension.swift
@@ -0,0 +1,32 @@
+//
+//  UIButton+Extension.swift
+//  Fillin-iOS
+//
+//  Created by Yi Joon Choi on 2022/01/11.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    func alignTextBelow(spacing: CGFloat = 8.0) {
+        guard let image = self.imageView?.image else {
+            return
+        }
+        
+        guard let titleLabel = self.titleLabel else {
+            return
+        }
+        
+        guard let titleText = titleLabel.text else {
+            return
+        }
+        
+        let titleSize = titleText.size(withAttributes: [
+            NSAttributedString.Key.font: titleLabel.font as Any
+        ])
+        
+        titleEdgeInsets = UIEdgeInsets(top: spacing, left: -image.size.width, bottom: -image.size.height, right: 0)
+        imageEdgeInsets = UIEdgeInsets(top: -(titleSize.height + spacing), left: 0, bottom: 0, right: -titleSize.width)
+    }
+}
