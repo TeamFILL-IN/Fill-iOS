4c491537ee1efc2e394f21bd2e9bdb843b07b692
dlwns33 <choi990406@gmail.com>
2022-01-12 22:22:15 +0900

    ✨[FEAT] #15 - 홈화면 테이블 뷰 구성

--- Fillin-iOS/Fillin-iOS/Sources/ViewControllers/HomeViewController/HomeViewController.swift
+++ Fillin-iOS/Fillin-iOS/Sources/ViewControllers/HomeViewController/HomeViewController.swift
@@ -9,15 +9,78 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    // MARK: - @IBOutlet Properties
+    @IBOutlet weak var homeTableView: UITableView!
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        registerXib()
     }
 
 }
 
+// MARK: - Extensions
 extension HomeViewController {
     private func setUI() {
         self.navigationController?.navigationBar.isHidden = true
+        homeTableView.dataSource = self
+        homeTableView.delegate = self
+        if #available(iOS 15, *) {
+            homeTableView.sectionHeaderTopPadding = 0
+        }
+        
     }
+    
+    private func registerXib() {
+        homeTableView.register(WelcomeTableViewCell.nib(), forCellReuseIdentifier: Const.Xib.welcomeTableViewCell)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row {
+        case 0 :
+            return 136
+        case 1 :
+            return 80
+        case 2 :
+            return 248
+        case 3 :
+            return 209
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0 :
+            guard let welcomeCell = tableView.dequeueReusableCell(withIdentifier: Const.Xib.welcomeTableViewCell, for: indexPath) as? WelcomeTableViewCell else {
+                return UITableViewCell()
+            }
+            
+            return welcomeCell
+//        case 1 :
+//            return 80
+//        case 2 :
+//            return 248
+//        case 3 :
+//            return 209
+        default:
+            return UITableViewCell()
+        }
+        
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension HomeViewController: UITableViewDelegate {
+    
 }
