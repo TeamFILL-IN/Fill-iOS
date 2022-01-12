141005219bae4947911d1189a0b47d886c368eef
dlwns33 <choi990406@gmail.com>
2022-01-12 22:24:55 +0900

    🚚[MOVE] #15 - 홈 뷰에 쓰이는 셀 그룹핑

---
+++ Fillin-iOS/Fillin-iOS/Sources/ViewControllers/HomeViewController/Cell/TabBarTableViewCell/TabBarTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TabBarTableViewCell.swift
+//  Fillin-iOS
+//
+//  Created by Yi Joon Choi on 2022/01/12.
+//
+
+import UIKit
+
+class TabBarTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
