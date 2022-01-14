39c9755a97bb24319ae871d0161b7597fe99168a
Jisoo Kim <kingwltn@naver.com>
2022-01-14 16:06:44 +0900

    ➕[ADD] - UIButton, UIView Extension 추가

--- Fillin-iOS/Fillin-iOS/Sources/ViewControllers/AddPhotoViewController/AddPhotoViewController.swift
+++ Fillin-iOS/Fillin-iOS/Sources/ViewControllers/AddPhotoViewController/AddPhotoViewController.swift
@@ -7,22 +7,86 @@
 
 import UIKit
 
-class AddPhotoViewController: UIViewController {
+import SnapKit
+import Then
 
+// MARK: - ADddPhotoViewController
+class AddPhotoViewController: UIViewController {
+  
+  // MARK: - Components
+  let navigationBar = FilinNavigationBar()
+  let photobackgroundView = UIView()
+  let photoIcon = UIButton()
+  let filmLabel = UILabel()
+  let filmchooseButton = UIButton()
+  let studioLabel = UILabel()
+  let studiochooseButton = UIButton()
+  let addphotoBackground = UIView()
+  let addphotoButton = UIButton()
+  
+  // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+      layout()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+// MARK: - Extension
+extension AddPhotoViewController {
+  func layout() {
+  }
+  func layoutNavigationBar() {
+    view.add(navigationBar) {
+      self.navigationBar.popViewController = {
+        self.navigationController?.popViewController(animated: true)
+      }
+      $0.snp.makeConstraints {
+        $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+        $0.leading.trailing.equalToSuperview()
+        $0.height.equalTo(50)
+      }
     }
-    */
-
+  }
+  func layoutPhotoBackgroundView() {
+    view.add(photobackgroundView) {
+      $0.backgroundColor = .darkGrey3
+      $0.snp.makeConstraints {
+        $0.top.equalTo(self.navigationBar.snp.bottom)
+        $0.centerX.equalToSuperview()
+        $0.width.height.equalTo(self.view.frame.width)
+      }
+    }
+  }
+  func layoutPhotoIcon() {
+    photobackgroundView.add(photoIcon) {
+      $0.setImage(UIImage(asset: Asset.icnEdit), for: .normal)
+      $0.snp.makeConstraints {
+        $0.centerX.equalToSuperview()
+        $0.centerY.equalToSuperview()
+        $0.width.equalTo(106)
+        $0.height.equalTo(106)
+      }
+    }
+  }
+  func layoutFilmLabel() {
+    view.add(filmLabel) {
+      $0.setupLabel(text: "Film",
+                    color: .fillinWhite,
+                    font: .body3)
+      $0.snp.makeConstraints {
+        $0.top.equalTo(self.photobackgroundView.snp.bottom).offset(30)
+        $0.leading.equalToSuperview().offset(18)
+      }
+    }
+  }
+  func layoutFilmChooseButton() {
+    view.add(filmchooseButton) {
+      $0.setupButton(title: "어떤 필름을 사용했나요?",
+                     color: .grey1,
+                     font: .body2,
+                     backgroundColor: .darkGrey2,
+                     state: .normal,
+                     radius: 0)
+      $0.setBorder(borderColor: .fillinRed, borderWidth: 1)
+    }
+  }
 }
