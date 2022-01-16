5be7008a2e61e7be66239c50cb2a8fac4cf94e96
dlwns33 <choi990406@gmail.com>
2022-01-17 04:27:53 +0900

    ✨[FEAT] #2 - 버튼 태그 적용 및 필름선택 tapGesture 적용

--- Fillin-iOS/Fillin-iOS/Sources/ViewControllers/FilmRollViewController/Cell/FilmTypeCollectionViewCell/FilmTypeCollectionViewCell.swift
+++ Fillin-iOS/Fillin-iOS/Sources/ViewControllers/FilmRollViewController/Cell/FilmTypeCollectionViewCell/FilmTypeCollectionViewCell.swift
@@ -14,9 +14,27 @@ class FilmTypeCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var chooseFilmView: UIView!
     @IBOutlet weak var chooseFilmLabel: UILabel!
     
+    // MARK: - @IBOutlet Properties
+    @IBAction func touchFilmTypeButtons(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        switch sender.tag {
+        case 0:
+            print(sender.tag)
+        case 1:
+            print(sender.tag)
+        case 2:
+            print(sender.tag)
+        case 3:
+            print(sender.tag)
+        default:
+            return
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         setUI()
+        setGesture()
     }
     
     // MARK: - Functions
@@ -29,5 +47,16 @@ class FilmTypeCollectionViewCell: UICollectionViewCell {
             $0.titleLabel?.font = .subhead2
         }
         chooseFilmLabel.font = .body2
+        filmTypeButtons[0].isSelected = true
     }
+    
+    private func setGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.touchChooseFilmView(_:)))
+        chooseFilmView.isUserInteractionEnabled = true
+        chooseFilmView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func touchChooseFilmView(_ sender: UITapGestureRecognizer) {
+         // TODO: 필름 종류 선택 뷰로 이동
+      }
 }
