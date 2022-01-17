1193be6253d25356238f1e510b963b711d25cfed
dlwns33 <choi990406@gmail.com>
2022-01-17 16:56:29 +0900

    ✨[FEAT] #48 - 헤더, 토큰 세팅

---
+++ Fillin-iOS/Fillin-iOS/Resources/Constants/Header.swift
@@ -0,0 +1,17 @@
+//
+//  Header.swift
+//  Fillin-iOS
+//
+//  Created by Yi Joon Choi on 2022/01/17.
+//
+
+import Foundation
+
+extension Const {
+    struct Header {
+//        static var tokenHeader = ["Content-Type": "application/json",
+//                                  "token": accessToken]
+        static var tokenHeader = ["Content-Type": "application/json",
+                                  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsImVtYWlsIjoiIiwiaWF0IjoxNjQyNDA1MDUyLCJleHAiOjE2NDM2MTQ2NTIsImlzcyI6ImZpbGxpbiJ9.NFD7MuGQmNcEw8l_NlWkxgz0Hx6KaQk4m4qwEDPuw5U"]
+    }
+}
---
+++ Fillin-iOS/Fillin-iOS/Resources/Constants/UserDefaultsKey.swift
@@ -0,0 +1,15 @@
+//
+//  UserDefaultsKey.swift
+//  Fillin-iOS
+//
+//  Created by Yi Joon Choi on 2022/01/17.
+//
+
+import Foundation
+
+extension Const {
+    struct UserDefaultsKey {
+        static let accessToken = "accessToken"
+        static let refreshToken = "refreshToken"
+    }
+}
