69e50503e95378f091fbee58b8c67bec6e220754
Jisoo Kim <kingwltn@naver.com>
2022-01-20 02:48:38 +0900

    ➕[ADD] #69 - Add Photo 뷰 필름선택, 스튜디오 선택 연결

--- Fillin-iOS/Fillin-iOS/Resources/Constants/Header.swift
+++ Fillin-iOS/Fillin-iOS/Resources/Constants/Header.swift
@@ -12,9 +12,9 @@ extension Const {
 //        static var tokenHeader = ["Content-Type": "application/json",
 //                                  "token": accessToken]
         static var tokenHeader = ["Content-Type": "application/json",
-                                  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJ0cGduczc3MDhAbmF2ZXIuY29tIiwiaWF0IjoxNjQyNjA4MDc5LCJleHAiOjE2NDM4MTc2NzksImlzcyI6ImZpbGxpbiJ9.ALonKj3zCrdoJtdYxdbPJe0-npZIKH3aBW6KSIuk-NI"]
+                                  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOm51bGwsImlhdCI6MTY0MjYxMTIzMSwiZXhwIjoxNjQzODIwODMxLCJpc3MiOiJmaWxsaW4ifQ.LDKIuHuEEJUuFVJWYfPJnemK24YUMepTcNjahR2TVd0"]
       static var multiTokenHeader = [
         "Content-Type": "multipart/form-data",
-                                "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJ0cGduczc3MDhAbmF2ZXIuY29tIiwiaWF0IjoxNjQyNjA4MDc5LCJleHAiOjE2NDM4MTc2NzksImlzcyI6ImZpbGxpbiJ9.ALonKj3zCrdoJtdYxdbPJe0-npZIKH3aBW6KSIuk-NI"]
+                                "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOm51bGwsImlhdCI6MTY0MjYxMTIzMSwiZXhwIjoxNjQzODIwODMxLCJpc3MiOiJmaWxsaW4ifQ.LDKIuHuEEJUuFVJWYfPJnemK24YUMepTcNjahR2TVd0"]
     }
 }
