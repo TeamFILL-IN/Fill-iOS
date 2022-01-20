39a00f2b2d9cdc12d049b8854f0b7a05d70b853b
dlwns33 <choi990406@gmail.com>
2022-01-20 12:03:11 +0900

    ✨[FEAT] #64 - API, Service 파일 연결

--- Fillin-iOS/Fillin-iOS/Sources/NetworkService/FilmRoll/FilmRollAPI.swift
+++ Fillin-iOS/Fillin-iOS/Sources/NetworkService/FilmRoll/FilmRollAPI.swift
@@ -31,6 +31,22 @@ public class FilmRollAPI {
         }
     }
     
+    func filmStylePhotos(styleId: Int, completion: @escaping (NetworkResult<Any>) -> Void) {
+        filmRollProvider.request(.filmStylePhotos(styleId: styleId)) { (result) in
+            switch result {
+            case .success(let response):
+                let statusCode = response.statusCode
+                let data = response.data
+                
+                let networkResult = self.judgeCurationStatus(by: statusCode, data)
+                completion(networkResult)
+                
+            case .failure(let err):
+                print(err)
+            }
+        }
+    }
+    
     private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
         
         let decoder = JSONDecoder()
