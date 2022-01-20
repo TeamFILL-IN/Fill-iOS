e543084d8a9908454e327cd65cce65a725c2dbe9
dlwns33 <choi990406@gmail.com>
2022-01-21 01:56:18 +0900

    🚑️[HOTFIX] - AddPhoto 잘못 올라간거 수정

--- Fillin-iOS/Fillin-iOS/Sources/ViewControllers/AddPhotoViewController/AddPhotoViewController.swift
+++ Fillin-iOS/Fillin-iOS/Sources/ViewControllers/AddPhotoViewController/AddPhotoViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 import SnapKit
 import Then
 import Photos
-import NVActivityIndicatorView
 
 // MARK: - ADddPhotoViewController
 class AddPhotoViewController: UIViewController {
@@ -40,67 +39,26 @@ class AddPhotoViewController: UIViewController {
     self.navigationController?.navigationBar.isHidden = true
     imagePickerController.delegate = self
     
-    // MARK: - Components
-    let navigationBar = FilinNavigationBar()
-    var photobackgroundView = UIImageView()
-    let photoIcon = UIButton()
-    let filmLabel = UILabel()
-    let filmchooseButton = UIButton()
-    let studioLabel = UILabel()
-    let studiochooseButton = UIButton()
-    let addphotoBackground = UIView()
-    let addphotoButton = UIButton()
-    let imagePickerController = UIImagePickerController()
+    /// 이미지뷰에 액션을 넣어서 이미지 선택하면 다시 사진 선택할 수 있게
+    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.touchimageView))
+    tapGestureRecognizer.isEnabled = true
+    tapGestureRecognizer.numberOfTapsRequired = 1
+    /// 이미지뷰는 터치가 원래 안되니까 터치 가능하도록
+    self.photobackgroundView.isUserInteractionEnabled = true
+    self.photobackgroundView.addGestureRecognizer(tapGestureRecognizer)
+    layout()
+    setNotification()
+  }
+  override func viewWillAppear(_ animated: Bool) {
+    self.filmchooseButton.titleLabel?.text = selectedFilm
     
-    var status: Status = .addPhotoVC
-    
-    var selectedId = 0
-    var selectedFilm = ""
-    
-    lazy var loadingBgView: UIView = {
-        let bgView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
-        bgView.backgroundColor = .backgroundCover
-        
-        return bgView
-    }()
-    
-    lazy var activityIndicator: NVActivityIndicatorView = {
-        let activityIndicator = NVActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 40, height: 40),
-                                                        type: .ballBeat,
-                                                        color: .fillinRed,
-                                                        padding: .zero)
-        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-        
-        return activityIndicator
-    }()
-    
-    // MARK: - LifeCycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .fillinBlack
-        self.navigationController?.navigationBar.isHidden = true
-        imagePickerController.delegate = self
-        
-        /// 이미지뷰에 액션을 넣어서 이미지 선택하면 다시 사진 선택할 수 있게
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.touchimageView))
-        tapGestureRecognizer.isEnabled = true
-        tapGestureRecognizer.numberOfTapsRequired = 1
-        /// 이미지뷰는 터치가 원래 안되니까 터치 가능하도록
-        self.photobackgroundView.isUserInteractionEnabled = true
-        self.photobackgroundView.addGestureRecognizer(tapGestureRecognizer)
-        layout()
-        setNotification()
+    // 이미지와 스튜디오가 있어야 버튼 활성화 되도록 분기처리
+    if (self.photobackgroundView.image != Asset.photoInsert.image) && (self.filmchooseButton.titleLabel?.text != "어떤 필름을 사용했나요?") {
+      self.addphotoBackground.backgroundColor = .fillinRed
+    } else {
+      self.addphotoBackground.backgroundColor = .darkGrey1
     }
-    override func viewWillAppear(_ animated: Bool) {
-        self.filmchooseButton.titleLabel?.text = selectedFilm
-        
-        // 이미지와 스튜디오가 있어야 버튼 활성화 되도록 분기처리
-        if (self.photobackgroundView.image != Asset.photoInsert.image) && (self.filmchooseButton.titleLabel?.text != "어떤 필름을 사용했나요?") {
-            self.addphotoBackground.backgroundColor = .fillinRed
-        } else {
-            self.addphotoBackground.backgroundColor = .darkGrey1
-        }
-    }
+  }
 }
 // MARK: - Extension
 extension AddPhotoViewController {
@@ -131,6 +89,12 @@ extension AddPhotoViewController {
         } else {
           self.navigationController?.popViewController(animated: true)
         }
+      }
+      $0.snp.makeConstraints {
+        $0.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+        $0.leading.trailing.equalToSuperview()
+        $0.height.equalTo(50)
+      }
     }
   }
   func layoutAddPhotoScrollView() {
@@ -242,17 +206,17 @@ extension AddPhotoViewController {
         $0.height.equalTo(74)
       }
     }
-    func layoutAddphotoButton() {
-        addphotoBackground.add(addphotoButton) {
-            $0.setTitle("Add Photo", for: .normal)
-            $0.titleLabel?.font = .engBighead
-            $0.setTitleColor(.grey3, for: .normal)
-            $0.addTarget(self, action: #selector(self.touchAddPhotoButton), for: .touchUpInside)
-            $0.snp.makeConstraints {
-                $0.top.equalToSuperview().offset(15)
-                $0.centerX.equalToSuperview()
-            }
-        }
+  }
+  func layoutAddphotoButton() {
+    addphotoBackground.add(addphotoButton) {
+      $0.setTitle("Add Photo", for: .normal)
+      $0.titleLabel?.font = .engBighead
+      $0.setTitleColor(.grey3, for: .normal)
+      $0.addTarget(self, action: #selector(self.touchAddPhotoButton), for: .touchUpInside)
+      $0.snp.makeConstraints {
+        $0.top.equalToSuperview().offset(15)
+        $0.centerX.equalToSuperview()
+      }
     }
   }
   @objc func touchimageView() {
@@ -278,51 +242,41 @@ extension AddPhotoViewController {
           if state == .authorized {
             self.imagePickerController.sourceType = .photoLibrary
             self.present(self.imagePickerController, animated: true, completion: nil)
-        case .notDetermined:
-            print("노 결정")
-            
-            PHPhotoLibrary.requestAuthorization({ state in
-                DispatchQueue.main.async {
-                    if state == .authorized {
-                        self.imagePickerController.sourceType = .photoLibrary
-                        self.present(self.imagePickerController, animated: true, completion: nil)
-                    } else {
-                        self.dismiss(animated: true, completion: nil)
-                    }
-                }
-            })
-            
-        default:
-            break
+          } else {
+            self.dismiss(animated: true, completion: nil)
+          }
         }
+      })
+      
+    default:
+      break
     }
-    @objc func touchFilmChooseButton() {
-        let filmSelectVC = FilmSelectViewController()
-        filmSelectVC.status = .addPhotoVC
-        self.navigationController?.pushViewController(filmSelectVC, animated: true)
+  }
+  @objc func touchFilmChooseButton() {
+    let filmSelectVC = FilmSelectViewController()
+    filmSelectVC.status = .addPhotoVC
+    self.navigationController?.pushViewController(filmSelectVC, animated: true)
+  }
+  @objc func touchStudioChooseButton() {
+    let studioChooseVC = StudioMapSearchViewController()
+    self.navigationController?.pushViewController(studioChooseVC, animated: true)
+  }
+  @objc func touchAddPhotoButton() {
+    if self.addphotoBackground.backgroundColor == .fillinRed {
+      addPhotosWithAPI(studioId: 6, filmId: selectedId, img: self.photobackgroundView.image ?? UIImage())
+    } else {
     }
-    @objc func touchStudioChooseButton() {
-        let studioChooseVC = StudioMapSearchViewController()
-        self.navigationController?.pushViewController(studioChooseVC, animated: true)
-    }
-    @objc func touchAddPhotoButton() {
-        if self.addphotoBackground.backgroundColor == .fillinRed {
-            
-            setActivityIndicator()
-            addPhotosWithAPI(studioId: 6, filmId: selectedId, img: self.photobackgroundView.image ?? UIImage())
-        } else {
-        }
-    }
-    // MARK: - Notification
-    private func setNotification() {
-        NotificationCenter.default.addObserver(self, selector: #selector(updateSelectedFilmType), name: Notification.Name.pushToAddPhotoViewController, object: nil)
-    }
-    @objc func updateSelectedFilmType(_ notification: Notification) {
-        let selectedFilmDict = notification.object as? NSDictionary
-        selectedId = selectedFilmDict?["selectedId"] as? Int ?? 0
-        selectedFilm = selectedFilmDict?["selectedFilm"] as? String ?? ""
-        self.filmchooseButton.setTitle(selectedFilm, for: .normal)
-    }
+  }
+  // MARK: - Notification
+  private func setNotification() {
+    NotificationCenter.default.addObserver(self, selector: #selector(updateSelectedFilmType), name: Notification.Name.pushToAddPhotoViewController, object: nil)
+  }
+  @objc func updateSelectedFilmType(_ notification: Notification) {
+    let selectedFilmDict = notification.object as? NSDictionary
+    selectedId = selectedFilmDict?["selectedId"] as? Int ?? 0
+    selectedFilm = selectedFilmDict?["selectedFilm"] as? String ?? ""
+    self.filmchooseButton.setTitle(selectedFilm, for: .normal)
+  }
 }
 
 // MARK: - ImagePicker Extension
@@ -332,31 +286,30 @@ extension AddPhotoViewController: UIImagePickerControllerDelegate, UINavigationC
       photobackgroundView.image = image
       photobackgroundView.contentMode = .scaleAspectFit
     }
+    dismiss(animated: true, completion: nil)
+  }
 }
 
 // MARK: - Network
 extension AddPhotoViewController {
-    func addPhotosWithAPI(studioId: Int, filmId: Int, img: UIImage) {
-        AddPhotoAPI.shared.addPhotos(studioId: studioId, filmId: filmId, img: img) { response in
-            switch response {
-            case .success:
-                self.activityIndicator.stopAnimating()
-                self.loadingBgView.removeFromSuperview()
-                
-                let secondVC = SecondAddPhotoPopUpViewController()
-                secondVC.modalPresentationStyle = .overCurrentContext
-                secondVC.modalTransitionStyle = .crossDissolve
-                self.present(secondVC, animated: false, completion: nil)
-            case .requestErr(let message):
-                print("addPhotosWithAPI - requestErr: \(message)")
-            case .pathErr:
-                print("addPhotosWithAPI - pathErr")
-            case .serverErr:
-                print("addPhotosWithAPI - serverErr")
-            case .networkFail:
-                print("addPhotosWithAPI - networkFail")
-            }
-        }
-        
+  func addPhotosWithAPI(studioId: Int, filmId: Int, img: UIImage) {
+    AddPhotoAPI.shared.addPhotos(studioId: studioId, filmId: filmId, img: img) { response in
+      switch response {
+      case .success:
+        let secondVC = SecondAddPhotoPopUpViewController()
+        secondVC.modalPresentationStyle = .overCurrentContext
+        secondVC.modalTransitionStyle = .crossDissolve
+        self.present(secondVC, animated: false, completion: nil)
+      case .requestErr(let message):
+        print("addPhotosWithAPI - requestErr: \(message)")
+      case .pathErr:
+        print("addPhotosWithAPI - pathErr")
+      case .serverErr:
+        print("addPhotosWithAPI - serverErr")
+      case .networkFail:
+        print("addPhotosWithAPI - networkFail")
+      }
     }
+    
+  }
 }
