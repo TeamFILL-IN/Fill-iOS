1fd3f693031b287ddc4f63bf308f09adef191320
KimJisoo <kingwltn@naver.com>
2022-09-07 15:01:41 +0900

    [FEAT] - #134 ReportVC, ReportDetailVC Layout 완료

--- Fillin-iOS/Fillin-iOS/Sources/ViewControllers/ReportViewController/ReportDetailViewController.swift
+++ Fillin-iOS/Fillin-iOS/Sources/ViewControllers/ReportViewController/ReportDetailViewController.swift
@@ -7,23 +7,23 @@
 
 import UIKit
 
-class ReportDetailViewController: UIViewController {
+import SnapKit
+import Then
 
+// MARK: - ReportDetailViewController
+class ReportDetailViewController: UIViewController {
+  
+  // MARK: - Components
+  
+
+  // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+}
+// MARK: - Extension
+extension ReportDetailViewController {
+  func layout() {
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
 }
